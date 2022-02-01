a5d8eadcca2a0c49c277dbf1243bbd4e911edb46
Diouf <fatou.diouf@cgi.com>
2022-02-01 11:14:00 -0500

    fixing things

---
+++ calculator-js/calculator-js/src/calc.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => a+b;
+
+
+//function sum (a,b){
+//    return a+b;
+//}
+
+//public int sum (int a, int b){     On Java, you have to declare the type of variable which is what I prefer
+//    return a+b;
+//}
+
+export {sum};
